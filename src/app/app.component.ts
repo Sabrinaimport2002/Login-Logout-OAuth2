831f6812bee9c7e9fc316a6c4503d392436c5213
Sabrina Vitória <sabrinaimport2002@gmail.com>
2022-10-17 08:22:00 -0300

    Adicionando componente de login, modificações de rota

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthConfig, JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+import { authCodeFlowConfig } from './login-logout.config';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Login-Logout';
+
+  constructor(private oauthService: OAuthService ){
+    this.configure();
+  }
+
+  configure(){
+    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    // this.oauthService.loadDiscoveryDocumentAndTryLogin();
+    this.oauthService.loadDiscoveryDocumentAndLogin();
+  }
+
+  // login(){
+  //   this.oauthService.initImplicitFlow();
+  // }
+
+  // logout(){
+  //   this.oauthService.logOut();
+  // }
+
+  get token(){
+    let claims: any;
+    claims = this.oauthService.getIdentityClaims();
+    return claims ? claims : null;
+  }
 }
